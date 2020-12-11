038c3ad86476267c61ae871820f17dee34ab73c1
mike <hurnhu5622@gmail.com>
2020-12-11 16:41:58 -0500

    working call from js and return

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	_ "./statik"
 	"github.com/rakyll/statik/fs"
 	"github.com/webview/webview"
+
+    "fmt"
+    "os"
 )
 
 // TODO: Replace with the absolute import path
-
+var test int
 func main() {
 
 	go start()
@@ -19,12 +22,15 @@ func main() {
 	debug := true
 	w := webview.New(debug)
 	defer w.Destroy()
+	w.Bind("goCode", handle);
+	w.Bind("stuff", test);
 	w.SetTitle("Minimal webview example")
 	w.SetSize(800, 600, webview.HintNone)
 	w.Navigate("http://localhost:8181/index.html")
 	w.Run()
 	println("m")
 }
+
 func start() {
 
 	statikFS, err := fs.New()
@@ -32,8 +38,23 @@ func start() {
 		log.Fatal(err)
 	}
 
-	println("ta")
 	// Serve the contents over HTTP.
 	http.Handle("/", http.StripPrefix("/", http.FileServer(statikFS)))
 	http.ListenAndServe(":8181", nil)
+}
+
+func handle(myData string) string{
+        f, err := os.Create("test.txt")
+        if err != nil {
+                fmt.Println(err)
+                return "err"
+        }
+        l, err := f.WriteString("Hello World")
+        if err != nil {
+                fmt.Println(err)
+                f.Close()
+                return "done"
+        }
+        println(l)
+        return "bad?"
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
-statik -src=./html
-go run main.go
+* statik -src=./html
+* go run main.go
+
+# windows
+* if blank screen run `CheckNetIsolation.exe LoopbackExempt -a -n="Microsoft.Win32WebViewHost_cw5n1h2txyewy"`
+* build on windows `go build -ldflags="-H windowsgui" -o webview-example.exe`
+* dll's must be rebuilt for `https://github.com/webview/webview` on local machine and dlls in same folder as exe
+* call go func from web `window.goCode("ping").then( (val) => console.log("asynchronous logging has val:",val) );`
